5d12d2f8bd679c3dc3f40ee2704f6c4f7ece65b2
Ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2024-02-18 22:22:38 +0000

    games tictactoe

---
+++ src/app/games/tictactoe/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from "react";
+
+import Confetti from "react-confetti";
+import Board from "./components/Board";
+type Props = {};
+
+const page = (props: Props) => {
+  const [boardSize, setBoardSize] = useState(3);
+  const [gameOver, setGameOver] = useState(false);
+  const [winnerString, setWinnerString] = useState("");
+
+  return (
+    <div className=" w-screen h-screen flex flex-col items-center p-6">
+      <h1 className=" text-4xl font-bold py-8">Tic Tac Toe</h1>
+      {!gameOver && (
+        <div>
+          <button
+            className=" min-w-[150px] text-lg px-10  m-2 py-2 rounded-md bg-sky-800"
+            onClick={() => setGameOver(true)}
+          >
+            Start
+          </button>
+        </div>
+      )}
+      {winnerString.length > 3 && (
+        <>
+          <Confetti />{" "}
+        </>
+      )}
+      {gameOver && (
+        <Board
+          boardSize={boardSize}
+          winnerString={winnerString}
+          setWinnerString={setWinnerString}
+        />
+      )}
+    </div>
+  );
+};
+
+export default page;
